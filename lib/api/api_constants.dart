9349b5ddab3d3430e44dd1b551cdbf078be4190d
Sujata Shrestha <90723058+sujata054@users.noreply.github.com>
2023-02-27 16:21:45 +0200

    Update api_constants.dart
    Api-key hidden

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   static const String baseUrl = "https://api.themoviedb.org/3/";
-  static const String apiKEY = "api_key=7abfbb6b0860ad039e46e7803c446a3a";
+  static const String apiKEY = " api-key: hidden";
   static const String baseImgUrl = "https://image.tmdb.org/t/p/w500";
 }
